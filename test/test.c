fc6442911279eb2f1cb4fa71289190016176f0a3
mts0629 <soratobi93@gmail.com>
2023-05-27 10:34:04 +0900

    Add test utiities

--- test/test.c
+++ test/test.c
@@ -4,6 +4,15 @@
 
 #include "b64.h"
 
+// Test utility macro
+#define ASSERT_STR_EQ(expected, actual) assert(strcmp((expected), (actual)) == 0)
+#define ASSERT_MEM_EQ(expected, actual, size) assert(memcmp((expected), (actual), (size)) == 0)
+
+#define RUN_TEST(test_case_func) { \
+    printf("%s\n", # test_case_func); \
+    (test_case_func)(); \
+}
+
 static uint8_t ORIGINAL_BYTES[] = "ABCDEFG";
 static char ENCODED_CHARS[] = "QUJDREVGRw==";
 
@@ -12,9 +21,7 @@ void test_encoding(void) {
 
     b64_encode(encoded_str, ORIGINAL_BYTES, sizeof(ORIGINAL_BYTES) - 1);
 
-    printf("%s\n", encoded_str);
-
-    assert(strcmp(ENCODED_CHARS, encoded_str) == 0);
+    ASSERT_STR_EQ(ENCODED_CHARS, encoded_str);
 }
 
 void test_decoding(void) {
@@ -22,7 +29,7 @@ void test_decoding(void) {
 
     assert(b64_decode(decoded_bytes, ENCODED_CHARS, sizeof(ENCODED_CHARS)) == 0);
 
-    assert(memcmp(ORIGINAL_BYTES, decoded_bytes, sizeof(ORIGINAL_BYTES)) == 0);
+    ASSERT_MEM_EQ(ORIGINAL_BYTES, decoded_bytes, sizeof(ORIGINAL_BYTES));
 }
 
 void test_decoding_fails_by_invalid_string(void) {
@@ -32,11 +39,11 @@ void test_decoding_fails_by_invalid_string(void) {
 }
 
 int main(void) {
-    test_encoding();
+    RUN_TEST(test_encoding);
 
-    test_decoding();
+    RUN_TEST(test_decoding);
 
-    test_decoding_fails_by_invalid_string();
+    RUN_TEST(test_decoding_fails_by_invalid_string);
 
     printf("Test finished\n");
 
