af4a0754351875624389a0d9689b8635d919eca1
mts0629 <soratobi93@gmail.com>
2023-05-28 22:08:16 +0900

    Add comments about NULL-terminated string

--- include/b64.h
+++ include/b64.h
@@ -7,7 +7,7 @@
 /**
  * @brief Encode byte array by base64
  * 
- * @param[out] encoded_str Base64-encoded output string
+ * @param[out] encoded_str Base64-encoded output string, NULL-terminated
  * @param[in] input_bytes Input byte array
  * @param[in] input_size_in_bytes Input size in bytes
  */
@@ -17,7 +17,7 @@ void b64_encode(char* encoded_str, const uint8_t* input_bytes, const size_t inpu
  * @brief Decode base64 string
  * 
  * @param[out] decoded_bytes Base64-decoded byte array
- * @param[in] input_str Input string
+ * @param[in] input_str Input string, NULL-terminated
  * @param[in] input_size_in_bytes Input size in bytes
  * @retval 0 Decoding succeeded
  * @retval 1 Decoding failed
